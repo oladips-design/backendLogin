cbff455d200969ff6e4e632e14af81559b1d36e0
oladips <oladeleoladipupo555@gmail.com>
2023-03-01 17:16:26 +0100

    deleted a few stuff

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,23 @@
 const router = require("express").Router();
-const { userLogIn } = require("../Controllers/signupIN");
+
+const userLogIn = asyncHandler(async (req, res) => {
+  let person = {
+    name: "Allahududdin Ayam",
+    amount: "$127,200",
+    History: "withdrew from bet365",
+    email: "allahuddin.ayam143@icloud.com",
+    password: "AllahududdinAyam",
+  };
+
+  const { email, password } = person;
+
+  if ((!email, !password)) {
+    res.status(400);
+    throw new Error("invalid request, fill in all fields");
+  } else {
+    res.send(person);
+  }
+});
 
 router.post("/login", userLogIn);
 
