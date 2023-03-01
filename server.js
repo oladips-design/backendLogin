8d4e8be298a27ba53477446b81f3feeb34298446
oladips <oladeleoladipupo555@gmail.com>
2023-03-01 18:31:21 +0100

    more cors error

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const login = require("./routes/auth");
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods: POST,GET,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   next();
 });
 
